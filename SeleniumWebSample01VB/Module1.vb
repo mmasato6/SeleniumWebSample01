20e5b4b8e4a26a01de1d13da0411f431a5444756
masato <mutsuya_masato@yahoo.co.jp>
2020-04-24 10:12:58 +0900

    Edgeの起動及び終了を実装

--- SeleniumWebSample01VB/Module1.vb
+++ SeleniumWebSample01VB/Module1.vb
@@ -1,7 +1,20 @@
-﻿Module Module1
+﻿Imports System.Threading
+Imports OpenQA.Selenium
+Imports OpenQA.Selenium.Edge
+
+Module Module1
 
     Sub Main()
-
+        Using webdriver = CreateWebDriver()
+            '結果を見たいので3秒待つ
+            Thread.Sleep(TimeSpan.FromSeconds(3))
+            'ブラウザ終了
+            webdriver.Quit()
+        End Using
     End Sub
 
+    Private Function CreateWebDriver() As IWebDriver
+        Dim edgeService = EdgeDriverService.CreateDefaultService()
+        Return New EdgeDriver
+    End Function
 End Module
