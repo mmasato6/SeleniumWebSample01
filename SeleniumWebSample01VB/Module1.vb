6872fad560ab89e4586f8d4ffffa24e70d08fa0d
masato <mutsuya_masato@yahoo.co.jp>
2020-04-24 10:13:58 +0900

    googleに遷移

--- SeleniumWebSample01VB/Module1.vb
+++ SeleniumWebSample01VB/Module1.vb
@@ -6,6 +6,10 @@ Module Module1
 
     Sub Main()
         Using webdriver = CreateWebDriver()
+
+            'googleに遷移
+            webdriver.Url = "https://www.google.co.jp"
+
             '結果を見たいので3秒待つ
             Thread.Sleep(TimeSpan.FromSeconds(3))
             'ブラウザ終了
