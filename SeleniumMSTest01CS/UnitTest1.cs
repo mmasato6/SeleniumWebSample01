1c50643259ea646a87d8da1ddd49514536644ef9
masato <mutsuya_masato@yahoo.co.jp>
2020-05-29 11:04:21 +0900

    操作結果を検証するコードを追加

--- SeleniumMSTest01CS/UnitTest1.cs
+++ SeleniumMSTest01CS/UnitTest1.cs
@@ -15,14 +15,38 @@ namespace SeleniumMSTest01CS
             using (IWebDriver webDriver = CreateWebDriver())
             {
                 // googleに遷移
-                webDriver.Url = @"https://www.google.co.jp";
+                webDriver.Navigate().GoToUrl(@"https://www.google.co.jp");
                 // CSSセレクタを使って要素を取得
                 IWebElement element = webDriver.FindElement(By.CssSelector("input[name=\"q\"]"));
                 // 取得した要素にテキストを入力してsubmit
-                element.SendKeys("selenium");
+                const string SearchWord = "selenium"; 
+                element.SendKeys(SearchWord);
                 element.Submit();
-                // 結果を見たいので3秒待つ
-                Thread.Sleep(TimeSpan.FromSeconds(3));
+                /*
+                //submitの直後ではURLもタイトルも変わっていない(=遷移が終わっていない(Seleniumの仕様？Edgeの仕様？Googleの仕様？))
+                Console.WriteLine(webDriver.Url);
+                Console.WriteLine(webDriver.Title);
+                */
+
+                //sleepしてからチェックすると遷移してるから、Submitが終わった後、ブラウザ側の処理が終わるまで待機する必要がある
+                //Thread.Sleep(3000);
+                //遷移が終わるまで最大3秒待機する
+                const string expectedTitle = "selenium - Google 検索";
+                //const string expectedTitle = "テストケース誤り";
+                OpenQA.Selenium.Support.UI.WebDriverWait wait = new OpenQA.Selenium.Support.UI.WebDriverWait(webDriver, TimeSpan.FromSeconds(3));
+                var transfered = wait.Until(e => e.Title == expectedTitle );
+                /*
+                Console.WriteLine(webDriver.Url);
+                Console.WriteLine(webDriver.Title);
+                */
+
+                // 最初のリンクの文字列に検索語句が含まれていることを確認する
+                var firstElement = webDriver.FindElement(By.ClassName("LC20lb")); //2020/05/29時点。
+                string actualText = firstElement.Text;
+                Console.WriteLine(actualText);
+                Assert.IsTrue(actualText.ToLower().Contains(SearchWord), $"actual text:{actualText}");
+                //Assert.IsTrue(actualText.ToLower().Contains("テストケース誤り"), $"actual text:{actualText}");
+
                 // ブラウザ終了
                 webDriver.Quit();
             }
