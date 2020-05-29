3c51eca28e463afdb6e5a1c5b16e5fee70cd4a49
masato <mutsuya_masato@yahoo.co.jp>
2020-05-29 10:06:48 +0900

    とりあえずコンソールアプリのコードをコピペ

--- SeleniumMSTest01CS/UnitTest1.cs
+++ SeleniumMSTest01CS/UnitTest1.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
 
 namespace SeleniumMSTest01CS
 {
@@ -9,6 +12,28 @@ namespace SeleniumMSTest01CS
         [TestMethod]
         public void TestMethod1()
         {
+            using (IWebDriver webDriver = CreateWebDriver())
+            {
+                // googleに遷移
+                webDriver.Url = @"https://www.google.co.jp";
+                // CSSセレクタを使って要素を取得
+                IWebElement element = webDriver.FindElement(By.CssSelector("input[name=\"q\"]"));
+                // 取得した要素にテキストを入力してsubmit
+                element.SendKeys("selenium");
+                element.Submit();
+                // 結果を見たいので3秒待つ
+                Thread.Sleep(TimeSpan.FromSeconds(3));
+                // ブラウザ終了
+                webDriver.Quit();
+            }
         }
+
+        private IWebDriver CreateWebDriver()
+        {
+            // Edge
+            var EdgeService = EdgeDriverService.CreateDefaultService();
+            return new EdgeDriver();
+        }
+
     }
 }
